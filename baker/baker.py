2ec3788bc49e749c73eb259ea844ba81ac0a04be
Sarah Usher <sarahnusher@gmail.com>
2024-06-26 19:13:01 +0100

    first test

--- baker/baker.py
+++ baker/baker.py
@@ -1,4 +1,7 @@
 def inc(x):
     return x + 2
 
+class Baker:
+    def cakes(self, ingredient, recipe):
+        return 0
 
--- test_baker/test_baker.py
+++ test_baker/test_baker.py
@@ -1,4 +1,15 @@
 from baker.baker import inc
+from baker.baker import Baker
 
 def test_answer():
     assert inc(3) == 5
+
+
+def test_return_0():
+    baker = Baker()
+    recipe = {"flour": 500}
+    ingredients = {"sugar": 500}
+    return_value = baker.cakes(recipe,ingredients)
+    assert return_value == 0
+
+
