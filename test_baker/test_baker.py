91b9817c2c79fb331b62de0cfbd3e7dbb8cbcdd6
smirnu <smirnulya@gmail.com>
2024-07-10 19:30:12 +0100

    some refactoring

--- test_baker/test_baker.py
+++ test_baker/test_baker.py
@@ -1,7 +1,7 @@
 from baker.baker import Baker
 
 
-def test_return_0():
+def test_missing_ingredients_return_0():
     baker = Baker()
     recipe = {"flour": 500}
     ingredients = {"sugar": 500}
@@ -9,7 +9,7 @@ def test_return_0():
     assert return_value == 0
 
 
-def test_return_1():
+def test_exact_amount_ingredients_return_1():
     baker = Baker()
     recipe = {"flour": 500}
     ingredients = {"flour": 500}
@@ -17,14 +17,14 @@ def test_return_1():
     assert return_value == 1;
 
 
-def test_run_2():
+def test_return_2():
     baker = Baker()
     recipe = {"flour": 500, "sugar": 200, "eggs": 1}
     ingredients = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
     return_value = baker.cakes(recipe, ingredients)
     assert return_value == 2;
 
-def test_run_0cakes():
+def test_ingredient_present_amount_0_return_0():
     baker = Baker()
     recipe = {"flour": 500, "sugar": 200, "eggs": 1}
     ingredients = {"flour": 0, "sugar": 1200, "eggs": 5, "milk": 200}
